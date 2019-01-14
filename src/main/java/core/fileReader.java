4583c453e07797f1a96a8d166aa8dfb1cd02a39a
Sergei Andreev <sergei.andreev.us@gmail.com>
2019-01-13 22:28:30 -0800

    TreeSet version

--- src/main/java/core/fileReader.java
+++ src/main/java/core/fileReader.java
@@ -5,34 +5,37 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class fileReader {
 	
-	static ArrayList readFile(String path) {
-		ArrayList content = new ArrayList();
+	static TreeSet readFile(String path) {
+		TreeSet content = new TreeSet();
+		//String content = null;
 		try {
 			FileReader fr = new FileReader(path);
 			BufferedReader br = new BufferedReader(fr);
-			if (br != null) {
-				System.out.println("Not Null");
-			}
-			while (br !=null) {
-				content.add(br.readLine().toString());
-				//content = br.readLine().toString();}
-			}
+			for (int i = 0; i <= content.size(); i++) {
+			content.add(br.readLine().toString());}
 		}
+		
 		catch (IOException e) {
 			System.out.println(e);
 		}
 		catch (NullPointerException a) {
-			System.out.println("File is empty " + a);
+			System.out.println("Null pointer " + a);
 		}
 		return content;
 	}
 	
 	public static void main (String[] args) {
 		String path ="/Users/sergei/qa.txt";
-		System.out.println(readFile(path));
+		//for (int i = 0; i < readFile(path).size(); i++) {
+		
+		Iterator set = readFile(path).iterator();
+		while (set.hasNext()) {
+		System.out.println(set.next());
 	}
-
+  }
 }
